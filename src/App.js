d8b9da80215f0f93b2a289cae1f0a92e118e9b31
gustavocgb11@gmail.com <gustavocgb11@gmail.com>
2022-09-12 13:30:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css'
+import './Main.css'
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
+// componente, estado, propriedade
 
 function App() {
+
+  
+
+  const [devs, setDevs] = useState([]);
+
+  
+
+  useEffect(() => {
+    async function loadDevs() {
+      const resp = await api.get('/devs');
+
+      setDevs(resp.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function submit(data) {
+    const resp = await api.post('/devs', data);
+    
+    // nao pode usar push
+    setDevs([...devs, resp.data]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={submit}/>
+      </aside>
+
+      <main>
+        <ul>          
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+        </ul>
+      </main>
+    </div> 
   );
 }
 
 export default App;
+
+// devs.map(dev => ( 'quando se usa (), como se fosse um return dentro do copor, declara o retorno da funcao, com {} é o corpo'
